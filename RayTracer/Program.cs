0234234fce564a5e7ad4e5c7cf8d8c4846fdd0c1
Nick Betsworth <nickbetsworth@users.noreply.github.com>
2022-06-29 20:52:51 +0100

    Slight change of scene.

--- RayTracer/Program.cs
+++ RayTracer/Program.cs
@@ -6,6 +6,7 @@ using RayTracer.Extensions;
 using RayTracer.Materials;
 using RayTracer.Utility;
 using Color = RayTracer.Data.Vector3;
+using Point3 = RayTracer.Data.Vector3;
 using Tracer = RayTracer.Tracer;
 
 if (args.Length == 0)
@@ -19,7 +20,7 @@ var tracerConfiguration = new TracerConfiguration
 {
     SamplesPerPixel = 100,
     MaxSampleDelta = 0.005,
-    MaxRayReflections = 75
+    MaxRayReflections = 50
 };
 
 var camera = new Camera
@@ -33,16 +34,16 @@ var camera = new Camera
 var materialBlueDiffuse = new LambertianMaterial(new Color(0.0, 0.25, 0.9));
 var materialMetal = new MetalMaterial(new Color(0.5, 0.5, 0.5));
 var materialMetalFuzz = new MetalMaterial(new Color(0.5, 0.5, 0.5), 0.75);
-var materialHollowGlass = new DielectricMaterial(1.5);
-var materialGround = new LambertianMaterial(new Color(0.1, 0.8, 0.1));
+var materialGlass = new DielectricMaterial(1.5);
+var materialGround = new LambertianMaterial(new Color(0.1, 0.1, 0.1));
 var materialDielectricDiamond = new DielectricMaterial(2.4);
 
 var scene = new Scene();
-scene.Add(new Sphere(new Color(0, 0, -1), 0.5, materialMetal));
-scene.Add(new Sphere(new Color(1, 0, -1), -0.5, materialHollowGlass));
-// scene.Add(new Sphere(new Color(2.5, 0, -2), -0.5, materialHollowGlass));
-scene.Add(new Sphere(new Color(-0.7, -0.25, -0.7), 0.25, materialBlueDiffuse));
-scene.Add(new Sphere(new Color(0, -100.5, -1), 100, materialGround));
+scene.Add(new Sphere(new Point3(1.0, 0.0, -1.0), 0.5, materialMetal));
+scene.Add(new Sphere(new Point3(-1.0, 0.0, -1.0), -0.4, materialGlass));
+scene.Add(new Sphere(new Point3(-1.0, 0.0, -1.0), 0.5, materialGlass));
+scene.Add(new Sphere(new Point3(0.0, 0.0, -1.0), 0.5, materialBlueDiffuse));
+scene.Add(new Sphere(new Point3(0.0, -100.5, -1.0), 100, materialGround));
 
 var tracer = new Tracer(tracerConfiguration, camera, scene);
 
